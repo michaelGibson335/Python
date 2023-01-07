c155f413c6082484f26179731def514b23898955
michaelGibson335 <berserker_lion@yahoo.com>
2023-01-07 14:30:31 -0500

    simpleEchoChatbot.py

---
+++ Projects/Chatbot/EchoChatbot/simpleEchoChatbot.py
@@ -0,0 +1,14 @@
+#this is a simple chatbot that echoes or repeats whatever a user inputs
+
+#greeting message from chatbot to user
+print('Hello I\'m Chatty the chatbox, welcome to this chatbox experience!')
+
+#explain to user what this program does
+print('What I\'m going to do is echo or repeat what you type to me')
+
+#prompt user to enter something for program to repeat in variable 'stuff_to_echo'
+stuff_to_echo = str(input('Please type me what you want me to repeat'))
+
+#output what user said
+print('You said: ' + stuff_to_echo )
+
