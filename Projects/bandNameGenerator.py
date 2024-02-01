9a73298fd1cfa74f2a3a2afcd213d735f8ca4660
Michael Gibson <berserker_lion@yahoo.com>
2024-01-31 21:10:19 -0500

    Pushing First Python Project!

--- Projects/bandNameGenerator.py
+++ Projects/bandNameGenerator.py
@@ -1,2 +1,9 @@
-#test
-print('Hello World!')
+#1. Create greeting for your program.
+print('Welcome to the band name generator.')
+#2. Ask the user for the city they grew up in.
+city = input('Which ciy did you grow up in? \n')
+print(city)
+#3. Ask the user for the name of a pet.
+pet = input('What is a name of a pet? \n')
+#4. Combine the name of their city and pet and show them their band name.
+print('Your band name could be ' + city + ' ' + pet)
