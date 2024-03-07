6a294a876b82bef3a77eab079d666736cea9ef6d
Michael Gibson <berserker_lion@yahoo.com>
2024-03-06 23:01:44 -0500

    Update to color random walk

--- Projects/MoreTurtleGraphics /RandomWalk/main.py
+++ Projects/MoreTurtleGraphics /RandomWalk/main.py
@@ -6,15 +6,24 @@ import random
 
 tom = t.Turtle()
 tom.shape('arrow')
+t.colormode(255)
+
+#fun to generate random numbers for r, g, b 
+def randomColors(): 
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    randomColors = (r, g, b)
+    return randomColors
 
 #lists containing colors and directions for lines to go(north, south, west, east)
-colors = ['CornflowerBlue', 'DarkOrchid', 'IndianRed', 'DeepSkyBlue', 'LightSeaGreen', 'wheat', 'SlateGray', 'SeaGreen']
+
 directions = [0, 90, 180, 270]
+tom.pensize(10)
 
 #loop to generate lines with colors and directions pulled from list above
 for i in range(200):
-    tom.color(random.choice(colors))
-    tom.pensize(10)
+    tom.color(randomColors())
     tom.forward(30)
     tom.setheading(random.choice(directions))
     
